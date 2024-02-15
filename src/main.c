fa092797fe35f3cb4bca30e607b6868a614eebcc
Joshua Cleverley <joshuacleverley@protonmail.com>
2024-02-15 14:24:23 +0000

    Return the keycode of key pressed, close if esc is pressed

--- src/main.c
+++ src/main.c
@@ -1,10 +1,21 @@
 #include <ncurses.h>
 
 int main() {
+  int ch;
+
   initscr();
-  printw("Hello, world!");
-  refresh();
-  getch();
+  raw();
+  keypad(stdscr, TRUE);
+  noecho();
+
+  while (true) {
+    ch = getch();
+    if (ch == 27)
+      break;
+    printw("Keycode: %d\n", ch);
+    refresh();
+  }
+
   endwin();
 
   return 0;
